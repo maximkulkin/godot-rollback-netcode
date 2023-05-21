425af06894a2fd43be5b235b4201d39b7aa13dbd
Maxim Kulkin <maxim.kulkin@gmail.com>
2023-05-21 01:24:33 -0400

    Remove extra line continuation in MessageSerializer

--- addons/godot-rollback-netcode/MessageSerializer.gd
+++ addons/godot-rollback-netcode/MessageSerializer.gd
@@ -4,7 +4,7 @@ static func is_type(obj: Object):
 	return Utils.has_interop_method(obj, "serialize_input") \
 		and Utils.has_interop_method(obj, "serialize_message") \
 		and Utils.has_interop_method(obj, "unserialize_input") \
-		and Utils.has_interop_method(obj, "unserialize_message") \
+		and Utils.has_interop_method(obj, "unserialize_message")
 
 const DEFAULT_MESSAGE_BUFFER_SIZE = 1280
 
